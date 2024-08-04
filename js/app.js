fa35483c447f996a90ddcc38d9714d04b68e9f39
emmanuelken <emmanuelsalken2014@gmail.com>
2024-08-04 15:12:49 +0100

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var Car = /** @class */ (function () {
+    function Car(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    Car.prototype.start = function () {
+        console.log("Car engine started");
+    };
+    return Car;
+}());
+var myCar = new Car("Toyota", "Camry", 2020);
+myCar.start();
+var motor = new Car("Lexus", "GX 460", 2021);
+myCar.start();
